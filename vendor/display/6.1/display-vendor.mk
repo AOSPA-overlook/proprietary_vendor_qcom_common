5c8f07a38eebc9c78f6bf7d971f40b91fc6712cc
BladeRunner-A2C <creator4983@aospa.co>
2025-07-31 23:10:39 +0200

    vendor: display: Introduce 6.1 component

---
+++ vendor/display/6.1/display-vendor.mk
@@ -0,0 +1,60 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/common/vendor/display/6.1
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/clstc_config_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/clstc_config_library.xml \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/display/DPU1010.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU1010.xml \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/display/DPU1020.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU1020.xml \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/display/DPU10__.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU10__.xml \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/display/DPU870.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU870.xml \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/display/DPU9__.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU9__.xml \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/display/thermallevel_to_fps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/thermallevel_to_fps.xml \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/init/feature_enabler_client.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/feature_enabler_client.rc \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/init/qdcmss.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/qdcmss.rc \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/init/vendor.qti.hardware.display.color-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.display.color-service.rc \
+    vendor/qcom/common/vendor/display/6.1/proprietary/vendor/etc/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
+
+PRODUCT_PACKAGES += \
+    libbacklight-calib \
+    libclstc_algorithm_adapter \
+    libclstc_gcp_adapter \
+    libclstc_tm \
+    libdigital-dimming \
+    libdisp-aba \
+    libdisplayqos \
+    libdisplayskuutils \
+    libdpp_manager \
+    libdpps \
+    libfeutils \
+    libgame_enhance \
+    libhdr_backlight_adapter \
+    libhdr_tm \
+    libintervmipc \
+    libmemutils \
+    libmm-hdcpmgr \
+    libqdcm-algo \
+    libqdcm-json-mode-parser \
+    libqdcm-mode-parser \
+    libqdutils \
+    libqrtrclient \
+    libqseed3 \
+    librcmask \
+    libsdm-color \
+    libsdm-colormgr-algo \
+    libsdm-disp-vndapis \
+    libsdmextension \
+    libsnapdragoncolor-manager \
+    libsnapdragoncolor-qdcm \
+    libtestutils \
+    libtinyxml2_1 \
+    libvmfilexfer \
+    vendor.qti.hardware.qdutils_disp@1.0 \
+    feature_enabler_client \
+    vendor.qti.hardware.display.color-service \
+    ppd \
+    qdcmss \
+    ubwcconvert
